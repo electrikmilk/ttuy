2e7eb7420b6b26a59978be9bb6ec993e19682b2d
electrikmilk <brandonjordan124@gmail.com>
2022-09-23 13:47:32 -0400

    Convert case of SGR constants to PascalCase
    This converts SGR constants to PascalCase while also renaming them
    slightly.

--- style.go
+++ style.go
@@ -9,65 +9,59 @@ import "fmt"
 type SGR int
 
 // Decorations
-const (
-	BOLD      SGR = 1
-	DIM           = 2
-	ITALIC        = 3
-	UNDERLINE     = 4
-	SLOWBLINK     = 5
-	FASTBLINK     = 6
-	INVERT        = 7
-	HIDE          = 8
-	CROSS         = 9
-	STOPBLINK     = 25
-	SHOW          = 28
-	STOPCROSS     = 29
-)
+
+const Bold SGR = 1
+const Dim SGR = 2
+const Italic SGR = 3
+const Underlined SGR = 4
+const Blink SGR = 5
+const BlinkFast SGR = 6
+const Inverted SGR = 7
+const Hidden SGR = 8
+const Crossed SGR = 9
 
 // Foreground Colors
-const (
-	BLACK          SGR = 30
-	RED                = 31
-	GREEN              = 32
-	YELLOW             = 33
-	BLUE               = 34
-	MAGENTA            = 35
-	CYAN               = 36
-	WHITE              = 37
-	BRIGHT_BLACK       = 90
-	BRIGHT_RED         = 91
-	BRIGHT_GREEN       = 92
-	BRIGHT_YELLOW      = 93
-	BRIGHT_BLUE        = 94
-	BRIGHT_MAGENTA     = 95
-	BRIGHT_CYAN        = 96
-	BRIGHT_WHITE       = 97
-)
+
+const Black SGR = 30
+const Red SGR = 31
+const Green SGR = 32
+const Yellow SGR = 33
+const Blue SGR = 34
+const Magenta SGR = 35
+const Cyan SGR = 36
+const White SGR = 37
+const LightBlack SGR = 90
+const LightRed SGR = 91
+const LightGreen SGR = 92
+const LightYellow SGR = 93
+const LightBlue SGR = 94
+const LightMagenta SGR = 95
+const LightCyan SGR = 96
+const LightWhite SGR = 97
 
 // Background Colors
-const (
-	BG_BLACK          SGR = 40
-	BG_RED                = 41
-	BG_GREEN              = 42
-	BG_YELLOW             = 43
-	BG_BLUE               = 44
-	BG_MAGENTA            = 45
-	BG_CYAN               = 46
-	BG_WHITE              = 47
-	BG_BRIGHT_BLACK       = 100
-	BG_BRIGHT_RED         = 101
-	BG_BRIGHT_GREEN       = 102
-	BG_BRIGHT_YELLOW      = 103
-	BG_BRIGHT_BLUE        = 104
-	BG_BRIGHT_MAGENTA     = 105
-	BG_BRIGHT_CYAN        = 106
-	BG_BRIGHT_WHITE       = 107
-)
+
+const BlackBg SGR = 40
+const RedBg SGR = 41
+const GreenBg SGR = 42
+const YellowBg SGR = 43
+const BlueBg SGR = 44
+const MagentaBg SGR = 45
+const CyanBg SGR = 46
+const WhiteBg SGR = 47
+const LightBlackBg SGR = 100
+const LightRedBg SGR = 101
+const LightGreenBg SGR = 102
+const LightYellowBg SGR = 103
+const LightBlueBg SGR = 104
+const LightMagentaBg SGR = 105
+const LightCyanBg SGR = 106
+const LightWhiteBg SGR = 107
 
 func Style(str string, styles ...SGR) (styled string) {
 	for _, s := range styles {
 		styled += fmt.Sprintf("\033[%dm", s)
 	}
-	styled += fmt.Sprintf("%s\033[0m", str)
+	styled += str + "\033[0m"
 	return
 }
