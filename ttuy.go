0fdeac6bdcde6920c6a792e0777facbd5e8709c5
electrikmilk <brandonjordan124@gmail.com>
2022-09-27 22:25:40 -0400

    Cleanup

--- ttuy.go
+++ ttuy.go
@@ -4,7 +4,21 @@
 
 package ttuy
 
-import "runtime"
+import (
+	"runtime"
+
+	"github.com/olekukonko/ts"
+)
+
+var rows int
+var cols int
+
+var upArrow = "\u2191"
+var downArrow = "\u2193"
+
+// var leftArrow = "\u2190"
+// var rightArrow = "\u2192"
+var bullet = "\u2022"
 
 func eol() (eol string) {
 	eol = "\n"
@@ -12,4 +26,14 @@ func eol() (eol string) {
 		eol = "\r\n"
 	}
 	return
+}
+
+func terminalCols() {
+	size, _ := ts.GetSize()
+	cols = size.Col() - 5
+}
+
+func terminalRows() {
+	size, _ := ts.GetSize()
+	rows = size.Row() - 3
 }
