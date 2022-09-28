a90817b7151e5c45b5bd0ceea6462f6f434d4f7f
electrikmilk <brandonjordan124@gmail.com>
2022-09-27 22:18:55 -0400

    Menu bug fix
    Call callback before we stop painting.

--- menu.go
+++ menu.go
@@ -103,9 +103,9 @@ func handleMenuKeys(key any) {
 			nextIndex++
 		}
 	case keyboard.KeyEnter:
-		stopPainting()
 		selectedOption := menuOptions[optionIndex]
 		selectedOption.Callback()
+		stopPainting()
 		return
 	}
 }
