09e19c10c971cfe69ee7c861e567f7d03b3380b7
Brandon Jordan <brandonjordan124@gmail.com>
2023-03-10 22:19:05 -0500

    Update spinner test

--- ttuy_test.go
+++ ttuy_test.go
@@ -179,31 +179,26 @@ func TestStylePersist(t *testing.T) {
 }
 
 func TestSpinner(t *testing.T) {
-	fmt.Println("Wait a tick")
 	go Spinner("Indeterminate Progress", Ticker)
 	for i := 0; i < 50; i++ {
 		time.Sleep(30 * time.Millisecond)
 	}
 	StopSpinner()
-	fmt.Println("Wait a tick")
 	go Spinner("Loading", DotDotDot)
 	for i := 0; i < 50; i++ {
 		time.Sleep(30 * time.Millisecond)
 	}
 	StopSpinner()
-	fmt.Println("Wait a tick")
 	go Spinner("Getting Ready", Throbber)
 	for i := 0; i < 50; i++ {
 		time.Sleep(30 * time.Millisecond)
 	}
 	StopSpinner()
-	fmt.Println("Wait a tick")
 	go Spinner("Installing", Blinker)
 	for i := 0; i < 50; i++ {
 		time.Sleep(30 * time.Millisecond)
 	}
 	StopSpinner()
-	fmt.Println("Wait a tick")
 }
 
 func TestPrompt(*testing.T) {
