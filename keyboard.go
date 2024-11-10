9bee5c77dd3deea63b2c0d6d55880f298776ab51
Brandon Jordan <brandonjordan124@gmail.com>
2024-11-10 13:28:42 -0500

    Cleanup keyboard abstraction

--- keyboard.go
+++ keyboard.go
@@ -14,50 +14,19 @@ var keyMap map[keyboard.Key]KeyBinding
 var listeningKeys = false
 var labels []string
 
+// KeyBinding describes a key binding to an action.
 type KeyBinding struct {
+	// Description of what the handler will do.
 	descriptor string
-	keyString  string
-	handler    func()
+
+	// Representation of key (e.g. ^C).
+	keyString string
+
+	// Called when the key is pressed.
+	handler func()
 }
 
-func BindKey(key keyboard.Key, binding KeyBinding) {
-	if !listeningKeys {
-		keyMap = make(map[keyboard.Key]KeyBinding)
-		initKeyboard()
-		listeningKeys = true
-	}
-
-	keyMap[key] = binding
-}
-
-func Hotkeys(separator string) string {
-	labels = []string{}
-	for _, keybind := range keyMap {
-		labels = append(labels, keyLabel(keybind))
-	}
-
-	slices.Sort(labels)
-
-	return strings.Join(labels, Style(separator, Dim))
-}
-
-func keyLabel(kb KeyBinding) string {
-	var label strings.Builder
-	label.WriteString(Style(kb.keyString, Dim))
-	label.WriteRune(' ')
-	label.WriteString(Style(kb.descriptor, GrayText))
-
-	return label.String()
-}
-
-func initKeyboard() {
-	go ReadKeys(func(key keyboard.Key) {
-		if binding, ok := keyMap[key]; ok {
-			binding.handler()
-		}
-	})
-}
-
+// ReadKeys abstracts the keyboard package.
 func ReadKeys(callback func(key keyboard.Key)) {
 	if err := keyboard.Open(); err != nil {
 		panic(err)
@@ -76,4 +45,44 @@ func ReadKeys(callback func(key keyboard.Key)) {
 			break
 		}
 	}
+}
+
+// BindKey binds key to binding.
+func BindKey(key keyboard.Key, binding KeyBinding) {
+	if !listeningKeys {
+		keyMap = make(map[keyboard.Key]KeyBinding)
+		initKeyboard()
+		listeningKeys = true
+	}
+
+	keyMap[key] = binding
+}
+
+func initKeyboard() {
+	go ReadKeys(func(key keyboard.Key) {
+		if binding, ok := keyMap[key]; ok {
+			binding.handler()
+		}
+	})
+}
+
+// PrintKeyBindings prints the registered key bindings between separator.
+func PrintKeyBindings(separator string) string {
+	labels = []string{}
+	for _, binding := range keyMap {
+		labels = append(labels, keyLabel(binding))
+	}
+
+	slices.Sort(labels)
+
+	return strings.Join(labels, Style(separator, Dim))
+}
+
+func keyLabel(kb KeyBinding) string {
+	var label strings.Builder
+	label.WriteString(Style(kb.keyString, Dim))
+	label.WriteRune(' ')
+	label.WriteString(Style(kb.descriptor, GrayText))
+
+	return label.String()
 }
